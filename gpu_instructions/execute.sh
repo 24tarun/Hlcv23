407775b53463f40d84055be05ad337b42e8926ec
tarunhariharan <tarun2k01@gmail.com>
2024-07-16 11:43:34 +0200

    updated path

--- gpu_instructions/execute.sh
+++ gpu_instructions/execute.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Path to the `.py` file you want to run
-PYTHON_SCRIPT_PATH="/home/hlcv_team004/Hlcv23/gpu_instructions/demo.py"
+PYTHON_SCRIPT_PATH="/home/hlcv_team004/Hlcv23/gpu_instructions"
 # Path to the Python binary of the conda environment
-CONDA_PYTHON_BINARY_PATH="/home/hlcv_team004/miniconda3/envs/hlcv-ss23/bin/python"
+CONDA_PYTHON_BINARY_PATH="/home/hlcv_team004/miniconda3/bin/python"
 
 cd $PYTHON_SCRIPT_PATH
 $CONDA_PYTHON_BINARY_PATH "$@"
